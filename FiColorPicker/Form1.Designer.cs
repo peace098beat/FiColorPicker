db5d27b254b2fed5a962ca8f8e731c276592375e
peace098beat <peace098beat.t@gmail.com>
2019-01-26 22:34:23 +0900

    First

--- FiColorPicker/Form1.Designer.cs
+++ FiColorPicker/Form1.Designer.cs
@@ -29,12 +29,190 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox2_Ave = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1_Ave = new System.Windows.Forms.PictureBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox_PickColor = new System.Windows.Forms.PictureBox();
+            this.label_PickColor = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2_Ave)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1_Ave)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_PickColor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(69, 60);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox2_Ave);
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1_Ave);
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Panel1MinSize = 150;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tabControl1);
+            this.splitContainer1.Size = new System.Drawing.Size(460, 151);
+            this.splitContainer1.SplitterDistance = 150;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // pictureBox2_Ave
+            // 
+            this.pictureBox2_Ave.Location = new System.Drawing.Point(78, 86);
+            this.pictureBox2_Ave.Name = "pictureBox2_Ave";
+            this.pictureBox2_Ave.Size = new System.Drawing.Size(58, 52);
+            this.pictureBox2_Ave.TabIndex = 1;
+            this.pictureBox2_Ave.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 86);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(69, 60);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1_Ave
+            // 
+            this.pictureBox1_Ave.Location = new System.Drawing.Point(74, 3);
+            this.pictureBox1_Ave.Name = "pictureBox1_Ave";
+            this.pictureBox1_Ave.Size = new System.Drawing.Size(55, 53);
+            this.pictureBox1_Ave.TabIndex = 0;
+            this.pictureBox1_Ave.TabStop = false;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(304, 149);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label_PickColor);
+            this.tabPage1.Controls.Add(this.pictureBox_PickColor);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(296, 123);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(296, 123);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // pictureBox_PickColor
+            // 
+            this.pictureBox_PickColor.Location = new System.Drawing.Point(6, 15);
+            this.pictureBox_PickColor.Name = "pictureBox_PickColor";
+            this.pictureBox_PickColor.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox_PickColor.TabIndex = 0;
+            this.pictureBox_PickColor.TabStop = false;
+            // 
+            // label_PickColor
+            // 
+            this.label_PickColor.AutoSize = true;
+            this.label_PickColor.Location = new System.Drawing.Point(7, 72);
+            this.label_PickColor.Name = "label_PickColor";
+            this.label_PickColor.Size = new System.Drawing.Size(35, 12);
+            this.label_PickColor.TabIndex = 1;
+            this.label_PickColor.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(460, 151);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2_Ave)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1_Ave)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_PickColor)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox1_Ave;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox pictureBox2_Ave;
+        private System.Windows.Forms.PictureBox pictureBox_PickColor;
+        private System.Windows.Forms.Label label_PickColor;
     }
 }
 
--- FiColorPicker/Form1.cs
+++ FiColorPicker/Form1.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace FiColorPicker
@@ -15,6 +12,116 @@ namespace FiColorPicker
         public Form1()
         {
             InitializeComponent();
+        }
+
+        int CaptureSize = 50;
+        int Zoom = 4;
+        int PictureBoxSize = 75;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Size = new Size(PictureBoxSize, PictureBoxSize);
+            pictureBox2.Size = new Size(PictureBoxSize, PictureBoxSize);
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int X = Cursor.Position.X;
+            int Y = Cursor.Position.Y;
+            X = X - CaptureSize / 2;
+            Y = Y - CaptureSize / 2;
+
+            // Screen Copy
+            Bitmap bmp = new Bitmap(CaptureSize, CaptureSize);
+            using (Graphics gg = Graphics.FromImage(bmp))
+            {
+                gg.CopyFromScreen(new Point(X, Y), new Point(0, 0), bmp.Size);
+            }
+            this.pictureBox1.Image = bmp;
+            this.pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            this.pictureBox1.Refresh();
+
+            // 画像処理
+            BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height),
+                                                ImageLockMode.ReadOnly,
+                                                PixelFormat.Format32bppArgb);
+
+            int r = 0; int g = 0; int b = 0; int a = 0;
+            byte[] buf = new byte[bmp.Width * bmp.Height * 4];
+            Marshal.Copy(data.Scan0, buf, 0, buf.Length);
+            double N = (double)buf.Length / 4.0;
+            for (int i = 0; i < buf.Length;)
+            {
+                b += (int)buf[i++];
+                g += (int)buf[i++];
+                r += (int)buf[i++];
+                a += (int)buf[i++];
+            }
+
+            r = (int)(r / N);
+            g = (int)(g / N);
+            b = (int)(b / N);
+            a = 1;
+
+            bmp.UnlockBits(data);
+            Color ave_color = Color.FromArgb(r, g, b);
+
+            this.pictureBox1_Ave.BackColor = ave_color;
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int X = Cursor.Position.X;
+            int Y = Cursor.Position.Y;
+            X = X - CaptureSize / 2 / Zoom;
+            Y = Y - CaptureSize / 2 / Zoom;
+
+            // Screen Copy
+            Bitmap bmp = new Bitmap(CaptureSize / Zoom, CaptureSize / Zoom);
+            using (Graphics gg = Graphics.FromImage(bmp))
+            {
+                gg.CopyFromScreen(new Point(X, Y), new Point(0, 0), bmp.Size);
+            }
+            this.pictureBox2.Image = bmp;
+            this.pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.Refresh();
+
+            // Center Color
+            Color CenterColor = bmp.GetPixel(bmp.Width / 2, bmp.Height / 2);
+            var H = CenterColor.GetHue();
+            var S = CenterColor.GetSaturation();
+            var B = CenterColor.GetBrightness();
+
+            this.Text = CenterColor.ToString();
+
+            // 画像処理
+            pictureBox2_Ave.BackColor = CenterColor;
+
+
+            //現在どのマウスボタンが押されているか調べる
+            if ((Control.ModifierKeys & Keys.Control) == Keys.Control)
+            {
+                if ((Control.MouseButtons & MouseButtons.Middle) == MouseButtons.Middle)
+                {
+                    Console.WriteLine("マウスの中央ボタンが押されています。" + Cursor.Position.ToString());
+
+                    pictureBox_PickColor.BackColor = CenterColor;
+                    label_PickColor.Text = CenterColor.ToString();
+                    label_PickColor.Text += String.Format("{0},{1},{2}", H, S, B);
+                }
+                if ((Control.MouseButtons & MouseButtons.Left) == MouseButtons.Left)
+                {
+                    Console.WriteLine("マウスの左ボタンが押されています。" + Cursor.Position.ToString());
+                }
+            }
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.Text = e.Location.ToString();
         }
     }
 }
